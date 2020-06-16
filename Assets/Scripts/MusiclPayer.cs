5592708ca745650656d223e22749ba1d17f6aa5f
Hijos <sebastianjrangel@gmail.com>
2020-06-16 11:53:10 +0200

    Tidying Checklist

--- Assets/Scripts/MusiclPayer.cs
+++ Assets/Scripts/MusiclPayer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusiclPayer : MonoBehaviour
 {
@@ -9,15 +8,5 @@ public class MusiclPayer : MonoBehaviour
     private void Awake() 
     {
       DontDestroyOnLoad(this.gameObject);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-      Invoke("LoadFirstLevel", 2f); 
-    }
-
-    private void LoadFirstLevel() 
-    {
-      SceneManager.LoadScene(1);
     }
 }
