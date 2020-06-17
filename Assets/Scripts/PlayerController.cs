e3b0adb8e2a4756f8237872cb6a813ab90afcaf5
Hijos <sebastianjrangel@gmail.com>
2020-06-17 10:51:00 +0200

    fixes scrpt

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Player : MonoBehaviour
+
+public class PlayerController : MonoBehaviour
 {
 
     float xThrow, yThrow;
